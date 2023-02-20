d4e6e0d01a2986f30044584423818627a92fc11a
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-02-20 12:53:48 -0600

    refactoring backup and update zshenv file
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- install.sh
+++ install.sh
@@ -3,56 +3,22 @@ CONFIG_DIR=~/.config
 MY_ZDOTDIR=$CONFIG_DIR/zdotdir
 ZDOTDIR_PLUGINS="$MY_ZDOTDIR/plugins"
 VIM_PLUGIN_PATH=$HOME/.vim/pack/plugins/start
+UTILS_PATH=$MY_ZDOTDIR/utils
+BACKUPS_PATH=$MY_ZDOTDIR/backups
 
-# Setting up ZDOTDIR
-set_zdotdir() {
-	echo ZDOTDIR=$MY_ZDOTDIR >> ~/.zshenv
-}
-
-get_backup_name() {
-	RESOURCE_NAME=$2
-	DATE=$(date +%F-%H_%M_%S)
-
-	echo "$RESOURCE_NAME_-$DATE.bkp"
-}
+source env/autoload_functions.sh $UTILS_PATH
 
 backup_and_set_zdotdir() {
-	echo Backing up ~/.zshenv into $MY_ZDOTDIR/backups
-	[[ ! -d $MY_ZDOTDIR/backups ]] && mkdir $MY_ZDOTDIR/backups || echo Skiping backup directory creation 
-	BACKUP_NAME=$(get_backup_name zshenv)
-	cp ~/.zshenv $BACKUP_NAME
+	BACKUP_NAME="$(backup_name "zshenv")"
+	backup_file ~/.zshenv $BACKUPS_PATH $BACKUP_NAME
 
-	sed -i -E 's/ZDOTDIR=.*//g'
-
-	set_zdotdir
-}
-
-get_project_name() {
-	FROM_PATH=$1
-
-	awk -F '/' '{print $NF}' <<< $FROM_PATH
-}
-
-clone_repo() {
-	GIT_REPO_PATH=$1
-    DEST_PATH=$2
-	PROJECT_NAME="$(get_project_name $GIT_REPO_PATH)"
-	FINAL_DEST_PATH="$2/$PROJECT_NAME"
-    GITHUB_URL="https://github.com"
-
-	echo Verifying local path: $FINAL_DEST_PATH
-    if [ ! -d $FINAL_DEST_PATH ]; then
-        echo "Cloning repository..."
-        git clone $GITHUB_URL/$GIT_REPO_PATH $FINAL_DEST_PATH
-    else
-        echo "Updating repository..."
-        git -C $FINAL_DEST_PATH pull
-    fi
-    echo "Installed on: $FINAL_DEST_PATH"
+	set_zdotdir "$MY_ZDOTDIR"
 }
 
 # init
 () {
+	echo "Installing zdotdir environment..."
+
 	REPOS_TO_CLONE=(
 		"abalamilla/zdotdir":$CONFIG_DIR
 		
@@ -83,4 +49,6 @@ clone_repo() {
 	done
 
 	[[ -z "${ZDOTDIR}" || $ZDOTDIR != $MY_ZDOTDIR ]] && backup_and_set_zdotdir || echo ZDOTDIR is already configured.
+
+	echo "Finish! Enjoy your new environment."
 }
