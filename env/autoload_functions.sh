fac1f4f7c54418e8a2211122cca2f30e58103eed
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-02-20 12:52:31 -0600

    added path parameter to load from other locations
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- env/autoload_functions.sh
+++ env/autoload_functions.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
+CURRENT_PATH="${0:a:h}"
+FUNCTIONS_PATH=${1:-$CURRENT_PATH}
 
-fpath=( ${0:a:h}/functions "${fpath[@]}" )
+fpath=( $FUNCTIONS_PATH/functions "${fpath[@]}" )
 autoload -Uz $fpath[1]/*(.:t)
 
 autoload -U compinit && compinit
