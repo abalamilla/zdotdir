df0b1f571f209e695266b7d1c621e893809a47db
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-02-20 09:58:08 -0600

    added some aliases
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

---
+++ env/config/aliases.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+# Bash
+if [[ "Darwin" == `uname` ]] ; then
+    alias ls='ls -FG'
+else
+    alias ls='ls -F --color=auto'
+fi
+
+alias la='ls -la'
+
+alias search='grep --color=auto --exclude-dir=node_modules'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Handy commands
+alias timestamp='date +%s'
+alias uuid='uuidgen | tr "[:upper:]" "[:lower:]"'
+
+# IMPORTANT: if you are using oh-my-zsh do not source .zshrc
+# https://github.com/ohmyzsh/ohmyzsh/wiki/FAQ#how-do-i-reload-the-zshrc-file
+# best options are restart terminal, `exec zsh` or omz reload
+alias zshreload=". ~/.zshrc"
+
+# git alias
+alias gst='git status'
+alias ga='git add'
+alias gp='git push'
+alias gl='git pull'
+alias gcsm='git commit --signoff -m'
+alias gcssm='git commit -S --signoff -m'
+alias gco='git checkout'
+alias gnb='git checkout -b'
+alias gpsu='git push --set-upstream'
+alias gd='git diff'
+alias gun='git restore --staged'
+alias grst='git restore'
