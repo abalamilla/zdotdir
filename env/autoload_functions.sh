a6c03eea5efbc5af9e1b24660f2ce275cf8dafe5
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-02-24 23:01:08 -0600

    added load brew completion
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- env/autoload_functions.sh
+++ env/autoload_functions.sh
@@ -3,6 +3,11 @@ CURRENT_PATH="${0:a:h}"
 FUNCTIONS_PATH=${1:-$CURRENT_PATH}
 
 fpath=( $FUNCTIONS_PATH/functions "${fpath[@]}" )
-autoload -Uz $fpath[1]/*(.:t)
+
+if [ ! -x "$(command -v $COMMAND)" ]; then
+	fpath=( "$(brew --repository)/share/zsh/site-functions" "${fpath[@]}" )
+fi
+
+for fp ($^fpath/*(.N,@)) autoload -Uz $fp
 
 autoload -U compinit && compinit
