dd2bc29f4c213dac01bc3d1cde9a0c5b0ed86849
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-02-24 22:13:50 -0600

    cleaning unused zstyles. added LESS variable to fix output encoding
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- zstyles/fzf-tab.sh
+++ zstyles/fzf-tab.sh
@@ -1,7 +1,4 @@
 #!/bin/zsh
-# preview
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath' # remember to use single quote here!!!
-
 # complete everything
 zstyle ':completion:*' fzf-search-display true
 
@@ -15,14 +12,8 @@ zstyle ':fzf-tab:complete:-command-:*' fzf-preview \
 # Homebrew
 zstyle ':fzf-tab:complete:brew-(install|uninstall|search|info):*-argument-rest' fzf-preview 'brew info $word'
 
-zstyle ':fzf-tab:complete:tldr:argument-1' fzf-preview 'tldr --color always $word'
-
 # show file contents
 zstyle ':fzf-tab:complete:*:*' fzf-preview 'less ${(Q)realpath}'
-export LESSOPEN='|$ZDOTDIR/.lessfilter %s'
+export LESSOPEN='| $ZDOTDIR/.lessfilter %s'
+export LESS='-r'
 
-# give a preview of commandline arguments when completing `kill`
-zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
-zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview \
-  '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
-zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
