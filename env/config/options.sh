150d79d1056226ac5b0eeb14881bbbf1fe8d01ad
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2023-08-17 17:54:15 -0600

    added python env load
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- env/config/options.sh
+++ env/config/options.sh
@@ -3,4 +3,8 @@
 HISTSIZE=10000
 SAVEHIST=10000
 
-PATH="/Applications/IntelliJ IDEA.app/Contents/MacOS:/opt/homebrew/opt/kubernetes-cli@1.22/bin:$PATH"
+PATH="/Applications/IntelliJ IDEA.app/Contents/MacOS:$PATH"
+
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
