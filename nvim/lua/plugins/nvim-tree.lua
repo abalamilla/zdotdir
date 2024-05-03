0d01fbcf3113dc2c191043ba2f4032761d28a92f
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-05-03 15:56:36 -0600

    open nvim tree on startup
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -36,5 +36,12 @@ return {
 				},
 			},
 		})
+
+		local function open_nvim_tree()
+			require("nvim-tree.api").tree.open()
+		end
+
+		-- open nvim-tree on startup
+		vim.api.nvim_create_autocmd({"VimEnter"}, { callback = open_nvim_tree })
 	end,
 }
