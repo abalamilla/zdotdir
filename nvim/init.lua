cc7adf723892628f359080af6ffb18aff84c9e7f
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-05-14 12:31:42 -0600

    add venv python executable path
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
+-- add python venv
+local py3nvim = vim.fn.expand("$ZDOTDIR/py3nvim/bin/python")
+vim.g.python_host_prog = py3nvim
+vim.g.python3_host_prog = py3nvim
+
 -- add ZDOTDIR/nvim to runtimepath
 vim.opt.rtp:prepend(vim.fn.expand("$ZDOTDIR/nvim"))
 
