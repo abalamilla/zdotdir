4514fdd0ba17a809e0bbcf8154095b787d4390e1
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-07-01 23:05:58 -0600

    telescope refactor and config
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,43 +1,69 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-telescope/telescope-fzf-native.nvim",
-	},
-	keys = {
-		{ "<leader>f", "<cmd>Telescope find_files<cr>" },
-		{ "<leader>b", "<cmd>Telescope buffers<cr>" },
-		{ "<leader>g", "<cmd>Telescope live_grep<cr>" },
-	},
-	config = function()
-		local telescope = require("telescope")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope-fzf-native.nvim",
+		},
+		keys = {
+			{ "<leader>f", "<cmd>Telescope find_files<cr>" },
+			{ "<leader>b", "<cmd>Telescope buffers<cr>" },
+			{ "<leader>g", "<cmd>Telescope live_grep<cr>" },
+		},
+		config = function()
+			local telescope = require("telescope")
 
-		telescope.setup {
-			defaults = {
-				layout_config = {
-					horizontal = {
-						preview_cutoff = 0,
+			telescope.setup {
+				defaults = {
+					layout_config = {
+						horizontal = {
+							preview_cutoff = 0,
+						},
+					},
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+						"--glob=!.git/",
 					},
 				},
-				vimgrep_arguments = {
-					"rg",
-					"--color=never",
-					"--no-heading",
-					"--with-filename",
-					"--line-number",
-					"--column",
-					"--smart-case",
-					"--hidden",
-					"--glob=!.git/",
-				},
-			},
-			pickers = {
-				find_files = {
-					hidden = true,
-				},
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				}
 			}
-		}
-	end,
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = 'make',
+		config = function()
+			require('telescope').load_extension('fzf')
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			-- This is your opts table
+			require("telescope").setup {
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {}
+					}
+				}
+			}
+			require("telescope").load_extension("ui-select")
+		end,
+	}
 }
