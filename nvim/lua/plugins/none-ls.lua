43fdd96f15be5aff8d16eaf21b80df65eef58249
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-07-02 22:34:28 -0600

    add none-ls
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

---
+++ nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,18 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.shfmt,
+
+				null_ls.builtins.diagnostics.eslint_d,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>df", vim.lsp.buf.format, {})
+	end,
+}
