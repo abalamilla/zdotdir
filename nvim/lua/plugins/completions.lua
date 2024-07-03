33d4cf542ac4e5893eccf7f179899670166bd79c
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-07-02 23:22:18 -0600

    add cmp lsp and formatting
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -1,5 +1,8 @@
 return {
 	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -22,12 +22,11 @@ return {
 				"rust_analyzer",
 			}
 
-			mason.setup {}
+			mason.setup({})
 
-			mason_lspconfig.setup {
+			mason_lspconfig.setup({
 				ensure_installed = lsp_ensure_installed,
-			}
-
+			})
 		end,
 	},
 	{
@@ -36,19 +35,20 @@ return {
 			local lspconfig = require("lspconfig")
 			local util = require("lspconfig.util")
 			local configs = require("lspconfig.configs")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.pyright.setup {}
-			lspconfig.yamlls.setup {}
-			lspconfig.helm_ls.setup {}
-			lspconfig.jsonls.setup {}
-			lspconfig.tsserver.setup {}
-			lspconfig.bashls.setup {}
-			lspconfig.lua_ls.setup {}
-			lspconfig.taplo.setup {}
-			lspconfig.terraformls.setup {}
-			lspconfig.markdown_oxide.setup {}
-			lspconfig.gopls.setup {}
-			lspconfig.rust_analyzer.setup {}
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.yamlls.setup({ capabilities = capabilities })
+			lspconfig.helm_ls.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.taplo.setup({ capabilities = capabilities })
+			lspconfig.terraformls.setup({ capabilities = capabilities })
+			lspconfig.markdown_oxide.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 
 			if not configs.kcl then
 				configs.kcl = {
@@ -60,21 +60,25 @@ return {
 				}
 			end
 
-			lspconfig.kcl.setup {}
+			lspconfig.kcl.setup({})
 		end,
 		keys = {
-			{ "K", ":lua vim.lsp.buf.hover()<CR>", "Hover" },
-			{ "gd", ":lua vim.lsp.buf.definition()<CR>", "Definition" },
-			{ "gD", ":lua vim.lsp.buf.declaration()<CR>", "Declaration" },
-			{ "gr", ":lua vim.lsp.buf.references()<CR>", "References" },
-			{ "gi", ":lua vim.lsp.buf.implementation()<CR>", "Implementation" },
-			{ "gs", ":lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
-			{ "gt", ":lua vim.lsp.buf.type_definition()<CR>", "Type Definition" },
-			{ "gR", ":lua vim.lsp.buf.rename()<CR>", "Rename" },
-			{ "<leader>wa", ":lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Workspace Folder" },
+			{ "K",          ":lua vim.lsp.buf.hover()<CR>",                   "Hover" },
+			{ "gd",         ":lua vim.lsp.buf.definition()<CR>",              "Definition" },
+			{ "gD",         ":lua vim.lsp.buf.declaration()<CR>",             "Declaration" },
+			{ "gr",         ":lua vim.lsp.buf.references()<CR>",              "References" },
+			{ "gi",         ":lua vim.lsp.buf.implementation()<CR>",          "Implementation" },
+			{ "gs",         ":lua vim.lsp.buf.signature_help()<CR>",          "Signature Help" },
+			{ "gt",         ":lua vim.lsp.buf.type_definition()<CR>",         "Type Definition" },
+			{ "gR",         ":lua vim.lsp.buf.rename()<CR>",                  "Rename" },
+			{ "<leader>wa", ":lua vim.lsp.buf.add_workspace_folder()<CR>",    "Add Workspace Folder" },
 			{ "<leader>wr", ":lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Workspace Folder" },
-			{ "<leader>wl", ":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", "List Workspace Folders" },
+			{
+				"<leader>wl",
+				":lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+				"List Workspace Folders",
+			},
 			{ "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", "Code Action" },
 		},
-}}
-
+	},
+}
