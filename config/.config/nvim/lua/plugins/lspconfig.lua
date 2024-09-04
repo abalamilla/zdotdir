3f29c2eab2f068956424d29806e0057f010a25c2
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-09-04 09:50:23 -0600

     show diagnostics on floating window when hover the cursor
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/plugins/lspconfig.lua
+++ config/.config/nvim/lua/plugins/lspconfig.lua
@@ -65,17 +65,21 @@ return {
 			end
 
 			lspconfig.kcl.setup({ capabilities = capabilities })
+
+			-- Show line diagnostics automatically in hover window
+			vim.o.updatetime = 250
+			vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 		end,
 		keys = {
-			{ "K",          ":lua vim.lsp.buf.hover()<CR>",                   "Hover" },
-			{ "gd",         ":lua vim.lsp.buf.definition()<CR>",              "Definition" },
-			{ "gD",         ":lua vim.lsp.buf.declaration()<CR>",             "Declaration" },
-			{ "gr",         ":lua vim.lsp.buf.references()<CR>",              "References" },
-			{ "gi",         ":lua vim.lsp.buf.implementation()<CR>",          "Implementation" },
-			{ "gs",         ":lua vim.lsp.buf.signature_help()<CR>",          "Signature Help" },
-			{ "gt",         ":lua vim.lsp.buf.type_definition()<CR>",         "Type Definition" },
-			{ "gR",         ":lua vim.lsp.buf.rename()<CR>",                  "Rename" },
-			{ "<leader>wa", ":lua vim.lsp.buf.add_workspace_folder()<CR>",    "Add Workspace Folder" },
+			{ "K", ":lua vim.lsp.buf.hover()<CR>", "Hover" },
+			{ "gd", ":lua vim.lsp.buf.definition()<CR>", "Definition" },
+			{ "gD", ":lua vim.lsp.buf.declaration()<CR>", "Declaration" },
+			{ "gr", ":lua vim.lsp.buf.references()<CR>", "References" },
+			{ "gi", ":lua vim.lsp.buf.implementation()<CR>", "Implementation" },
+			{ "gs", ":lua vim.lsp.buf.signature_help()<CR>", "Signature Help" },
+			{ "gt", ":lua vim.lsp.buf.type_definition()<CR>", "Type Definition" },
+			{ "gR", ":lua vim.lsp.buf.rename()<CR>", "Rename" },
+			{ "<leader>wa", ":lua vim.lsp.buf.add_workspace_folder()<CR>", "Add Workspace Folder" },
 			{ "<leader>wr", ":lua vim.lsp.buf.remove_workspace_folder()<CR>", "Remove Workspace Folder" },
 			{
 				"<leader>wl",
