19c7c8034235d7c1663e65b04633e6e9250f92dc
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-12-11 12:21:56 -0600

    📦️ add krew
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- env/config/options.sh
+++ env/config/options.sh
@@ -8,6 +8,7 @@ setopt histignorespace
 setopt histignorealldups
 
 PATH="/Applications/IntelliJ IDEA.app/Contents/MacOS:$PATH"
+PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
 
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
