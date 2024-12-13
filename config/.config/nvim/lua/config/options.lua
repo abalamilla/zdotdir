acef0571b829e5c7da49fc03b7948816e3b4263d
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2024-12-13 15:42:08 -0600

    🛠️ vim enable virtual edit
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -12,6 +12,7 @@ opt.wildmenu = true
 opt.showcmd = true
 opt.autoindent = true
 opt.wrap = true
+opt.virtualedit = "all"
 
 -- add python venv
 local venv = vim.fn.expand("$HOME/zdotdir/.venv/bin/python")
