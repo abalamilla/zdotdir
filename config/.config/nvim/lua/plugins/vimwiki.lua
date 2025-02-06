8c2fb129534a5f26d65a38e968def324f1dc37d1
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-02-06 16:14:05 -0600

    ⚙ vimwiki config update, split personal and work wiki
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/plugins/vimwiki.lua
+++ config/.config/nvim/lua/plugins/vimwiki.lua
@@ -1,3 +1,19 @@
 return {
-	'vimwiki/vimwiki',
+  "vimwiki/vimwiki",
+  init = function()
+    vim.g.vimwiki_auto_header = 1
+    vim.g.vimwiki_listing_hl = 1
+    vim.g.vimwiki_global_ext = 0
+
+    vim.g.vimwiki_list = {
+      {
+        path = "~/vimwiki/ab/",
+        diary_frequency = "weekly",
+      },
+      {
+        path = "~/vimwiki/pgd/",
+        diary_frequency = "weekly",
+      }
+    }
+  end,
 }
