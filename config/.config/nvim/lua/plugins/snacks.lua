64fd345d7b60af4210a078c8637a13e8422cc6df
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-02-14 22:05:34 -0600

    🔃🌳 replace neo-tree with Snacks Explorer
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/plugins/snacks.lua
+++ config/.config/nvim/lua/plugins/snacks.lua
@@ -19,5 +19,39 @@ return {
         header = logo,
       },
     },
+    explorer = {
+      replace_netrw = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          layout = { layout = { position = "right" } },
+          auto_close = true,
+          on_change = function(picker)
+            local cwd = picker:cwd()
+            local actual_pwd = vim.fn.getcwd()
+
+            if cwd ~= actual_pwd then
+              vim.cmd("cd " .. cwd)
+            end
+          end,
+          hidden = true,
+          win = {
+            list = {
+              wo = {
+                number = true,
+              },
+            },
+          },
+        },
+        files = {
+          hidden = true,
+          follow = true,
+          filter = {
+            cwd = true,
+          },
+        },
+      },
+    },
   },
 }
