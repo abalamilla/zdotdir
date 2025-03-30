4a642f2b0a2b0ec357a7be25f5970e791fdabfd7
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-03-29 21:23:04 -0600

    🚀 code refactor
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/aerospace/on-ws-change.sh
+++ config/.config/aerospace/on-ws-change.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-chrome_pip=$(aerospace list-windows --all --format '%{window-id}|%{window-title}|' | rg "Picture in Picture" | awk -F'|' 'NR==1{print $1}')
-outlook_reminder=$(aerospace list-windows --all --format '%{window-id}|%{window-title}' | rg "Reminder" | awk -F'|' 'NR==1{print $1}')
-ws=$(aerospace list-workspaces --focused)
+CURRENT_WS=$(aerospace list-workspaces --focused)
 
-[[ $chrome_pip != "" ]] && aerospace move-node-to-workspace --window-id "${chrome_pip}" "${ws}"
-[[ $outlook_reminder != "" ]] && aerospace move-node-to-workspace --window-id "${outlook_reminder}" "${ws}"
+move_window() {
+	WINDOW_TITLE=$1
+
+	WINDOW_ID=$(aerospace list-windows --all --format '%{window-id}|%{window-title}|' | rg "${WINDOW_TITLE}" | awk -F'|' 'NR==1{print $1}')
+
+	[[ $WINDOW_ID != "" ]] && aerospace move-node-to-workspace --window-id "${WINDOW_ID}" "${CURRENT_WS}"
+}
+
+move_window "Picture in Picture"
+move_window "Reminder"
