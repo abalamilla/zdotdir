2c6e4dd61eb82ee181eb97426b48dd58d1e6fc30
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-03-30 10:58:33 -0600

    🚀 use aerospace focused workspace env var
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/aerospace/on-ws-change.sh
+++ config/.config/aerospace/on-ws-change.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-CURRENT_WS=$(aerospace list-workspaces --focused)
-
 move_window() {
 	WINDOW_TITLE=$1
 
 	WINDOW_ID=$(aerospace list-windows --all --format '%{window-id}|%{window-title}|' | rg "${WINDOW_TITLE}" | awk -F'|' 'NR==1{print $1}')
 
-	[[ $WINDOW_ID != "" ]] && aerospace move-node-to-workspace --window-id "${WINDOW_ID}" "${CURRENT_WS}"
+	[[ $WINDOW_ID != "" ]] && aerospace move-node-to-workspace --window-id "${WINDOW_ID}" "${AEROSPACE_FOCUSED_WORKSPACE}"
 }
 
 move_window "Picture in Picture"
