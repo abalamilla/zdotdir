abc7eaa8969f210bbd66fb4702622116e2b96e56
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-04-01 11:24:42 -0600

    🚀 move only between external monitors
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/aerospace/on-ws-change.sh
+++ config/.config/aerospace/on-ws-change.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+BUILT_IN=$(aerospace list-monitors | rg "Built-in" | awk -F'|' '{print $1}' | xargs )
+FOCUSED_MONITOR=$(aerospace list-monitors --focused | awk -F'|' '{print $1}' | xargs)
+
 move_window() {
 	WINDOW_TITLE=$1
 
 	WINDOW_ID=$(aerospace list-windows --all --format '%{window-id}|%{window-title}|' | rg "${WINDOW_TITLE}" | awk -F'|' 'NR==1{print $1}')
 
-	[[ $WINDOW_ID != "" ]] && aerospace move-node-to-workspace --window-id "${WINDOW_ID}" "${AEROSPACE_FOCUSED_WORKSPACE}"
+	[[ $WINDOW_ID != "" && $FOCUSED_MONITOR != "$BUILT_IN" ]] && aerospace move-node-to-workspace --window-id "${WINDOW_ID}" "${AEROSPACE_FOCUSED_WORKSPACE}"
 }
 
 move_window "Picture in Picture"
