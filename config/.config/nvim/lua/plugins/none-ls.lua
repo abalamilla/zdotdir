368640a8469b6f5b9a0a0ba17e603654d3572fde
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-08-26 09:02:15 -0600

    🧰 add none-ls config
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

---
+++ config/.config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,14 @@
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    local nls = require("null-ls")
+    opts.sources = opts.sources or {}
+    table.insert(opts.sources, nls.builtins.formatting.prettier.with({
+      filetypes = { "markdown" },
+      extra_args = {
+        "--prose-wrap", "always",
+        "--print-width", "80",
+      },
+    }))
+  end,
+}
