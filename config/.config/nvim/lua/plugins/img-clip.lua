79fe9c7843e97d15e19420868f54153f80929f98
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-09-03 19:36:29 -0600

    🛠️ add nvim img-clip plugin
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

---
+++ config/.config/nvim/lua/plugins/img-clip.lua
@@ -0,0 +1,10 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+  },
+  keys = {
+    -- suggested keymap
+    { "<leader>I", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
+}
