3fee32af3d42985b6672c8036c58a1b0750a0f15
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-09-08 13:51:01 -0600

    🛠️ add nvim diffview plugin
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

---
+++ config/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,31 @@
+local close_panel = { "n", "<esc>", "", { desc = "Close file panel" } }
+
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gdvc", ":DiffviewClose<CR>", desc = "Close Diffview" },
+    { "<leader>gdvo", ":DiffviewOpen<CR>", desc = "Open Diffview" },
+    { "<leader>gdvl", ":DiffviewLog<CR>", desc = "Open Diffview Log" },
+    { "<leader>gdvh", ":DiffviewFileHistory<CR>", desc = "Open Diffview File History" },
+    {
+      "<leader>gdvg",
+      function()
+        local grep_pattern = vim.fn.input("Grep pattern: ")
+        if grep_pattern ~= "" then
+          vim.cmd("DiffviewFileHistory --grep=" .. grep_pattern)
+        end
+      end,
+      desc = "Diffview File History with grep",
+    },
+  },
+  opts = {
+    keymaps = {
+      file_panel = {
+        close_panel,
+      },
+      file_history_panel = {
+        close_panel,
+      },
+    },
+  },
+}
