b337f3e408c6e4d0a84a56cc48ec003a5fdd6c3e
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2025-11-26 16:52:19 -0600

    🖌️ add nvim color schemes
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/plugins/colorscheme.lua
+++ config/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,28 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "gruvbox",
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      transparent = false,
+      theme = "wave", -- wave, dragon, lotus
+    },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main", -- main, moon, dawn
+      dark_variant = "main",
+    },
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "soft", -- can be "hard", "soft" or empty string
     },
   },
 }
