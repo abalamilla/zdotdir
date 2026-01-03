5bb6492c4b4960f037bb18fcd8a0c835334cc317
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2026-01-03 12:34:50 -0600

    🧰 add keymap to select icons when insert mode
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Icon picker in insert mode
+vim.keymap.set("i", "<C-i>", function()
+  Snacks.picker.icons()
+end, { desc = "Pick icon" })
