5ed414c2bdfde43b67e083696e654b7fd2037998
Abraham Alamilla <abraham.alamilla@aalamilla.com>
2026-06-09 17:02:34 -0600

    🧰 disable claude code provider
    Signed-off-by: Abraham Alamilla <abraham.alamilla@aalamilla.com>

--- config/.config/nvim/lua/plugins/claude-code.lua
+++ config/.config/nvim/lua/plugins/claude-code.lua
@@ -1,32 +1,8 @@
-local toggle_key = "<C-,>"
 return {
   "coder/claudecode.nvim",
-  dependencies = { "folke/snacks.nvim" },
-  config = true,
   opts = {
     terminal = {
-      snacks_win_opts = {
-        position = "float",
-        width = 0.85,
-        height = 0.85,
-        border = "rounded",
-        keys = {
-          {
-            toggle_key,
-            function(self)
-              self:hide()
-            end,
-            mode = "t",
-            desc = "Hide (Ctrl+,)",
-          },
-        },
-      },
+      provider = "none"
     },
-    diff_opts = {
-      keep_terminal_focus = true,
-    },
-  },
-  keys = {
-    { toggle_key, "<cmd>ClaudeCodeFocus<cr>", desc = "Claude Code", mode = { "n", "x" } },
-  },
+  }
 }
